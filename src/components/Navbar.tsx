0d224ad23be1363b4df0cc4ec668f39fb22fbc32
Braskin Ulloa <braskin.ulloa@gmail.com>
2023-07-27 16:37:45 +0200

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-item-list">
+        <div className="navbar-item">HOME</div>
+        <div className="navbar-item">ABOUT</div>
+        <div className="navbar-item">PORTFOLIO</div>
+        <div className="navbar-item">CONTACT</div>
+      </div>
+    </div>
+  );
+};
---
+++ src/views/Home.tsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="home-view">
+      <div className="home-left">
+        <div className="intro">
+          <span className="hello">HELLO,</span> <br />I AM A{" "}
+          <span className="profession">{"<b>WEB DEVELOPER</b>"}</span> <br />
+          <span className="name">BRASKIN ULLOA</span>
+        </div>
+        <div></div>
+      </div>
+      <div className="home-right">8x4</div>
+    </div>
+  );
+};
